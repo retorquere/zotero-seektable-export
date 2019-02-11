812360ebddca18559ef25df5a605bae7875692ff
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-02-11 14:06:40 +0100

    cleanup

--- build.js
+++ build.js
@@ -3,20 +3,21 @@ const tsconfig = require('./tsconfig.json')
 const pkg = require('./package.json')
 
 const translator = {
-  source: 'SeekTable.ts',
-  target: `${tsconfig.compilerOptions.outDir}/SeekTable.js`,
+  source: `${pkg.exporter}.ts`,
+  target: `${tsconfig.compilerOptions.outDir}/${pkg.exporter}.js`,
 }
 translator.mtime = fs.statSync(translator.source).mtime
 translator.data = fs.readFileSync(translator.target, 'utf-8')
 
 const header = {
-  path: 'SeekTable.json',
+  path: `${pkg.exporter}.json`,
 }
 header.mtime = fs.statSync(header.path).mtime
 if (header.mtime < translator.mtime) header.mtime = translator.mtime
 
 header.data = require(`./${header.path}`)
 header.data.lastUpdated = header.mtime.toISOString().replace('T', ' ').replace(/\..*/, '')
+header.label = pkg.exporter
 header.data = JSON.stringify(header.data, null, 2)
 
 version = `// version: ${pkg.version}`
