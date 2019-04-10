773a5af81802e845a54b03fdbf1495ea44e7923e
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-04-10 12:48:32 +0200

    FODS

--- SeekTable.ts
+++ SeekTable.ts
@@ -49,15 +49,24 @@ const aliases = Object.entries({
   dictionaryTitle: 'publicationTitle',
 })
 
-function quote(value) {
-  if (typeof value === 'number') return `${value}`
-  if (!value) return ''
-  if (!value.match(/[,"]/)) return value
-  return `"${value.replace(/"/g, '""')}"`
+function make_cell(value) {
+  if (typeof value === 'number') value = `${value}`
+  if (!value) value = ''
+
+  const entity = {
+    '"': '&quot;',
+    "'": '&apos;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '&': '&amp;',
+  }
+
+  value = value.split('\n').map(text => `<text:p>${text.replace(/[<>"'&]/g, c => entity[c])}</text:p>`).join('')
+  return `<table:table-cell office:value-type="string" calcext:value-type="string">${value}</table:table-cell>`
 }
 
 function make_row(cells) {
-  return cells.map(quote).join(',') + '\r\n'
+  return `<table:table-row>${cells.map(make_cell).join('')}</table:table-row>`
 }
 
 function makeGenerator(type) {
@@ -205,11 +214,31 @@ function doExport() {
     }
   }
   headers.sort()
+
+  Zotero.write(`
+    <?xml version='1.0' encoding='UTF-8'?>
+    <office:document xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" xmlns:draw="urn:oasis:names:tc:opendocument:xmlns:drawing:1.0" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:meta="urn:oasis:names:tc:opendocument:xmlns:meta:1.0" xmlns:number="urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0" xmlns:presentation="urn:oasis:names:tc:opendocument:xmlns:presentation:1.0" xmlns:svg="urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0" xmlns:chart="urn:oasis:names:tc:opendocument:xmlns:chart:1.0" xmlns:dr3d="urn:oasis:names:tc:opendocument:xmlns:dr3d:1.0" xmlns:math="http://www.w3.org/1998/Math/MathML" xmlns:form="urn:oasis:names:tc:opendocument:xmlns:form:1.0" xmlns:script="urn:oasis:names:tc:opendocument:xmlns:script:1.0" xmlns:config="urn:oasis:names:tc:opendocument:xmlns:config:1.0" xmlns:ooo="http://openoffice.org/2004/office" xmlns:ooow="http://openoffice.org/2004/writer" xmlns:oooc="http://openoffice.org/2004/calc" xmlns:dom="http://www.w3.org/2001/xml-events" xmlns:xforms="http://www.w3.org/2002/xforms" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:rpt="http://openoffice.org/2005/report" xmlns:of="urn:oasis:names:tc:opendocument:xmlns:of:1.2" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:grddl="http://www.w3.org/2003/g/data-view#" xmlns:tableooo="http://openoffice.org/2009/table" xmlns:drawooo="http://openoffice.org/2010/draw" xmlns:calcext="urn:org:documentfoundation:names:experimental:calc:xmlns:calcext:1.0" xmlns:loext="urn:org:documentfoundation:names:experimental:office:xmlns:loext:1.0" xmlns:field="urn:openoffice:names:experimental:ooo-ms-interop:xmlns:field:1.0" xmlns:formx="urn:openoffice:names:experimental:ooxml-odf-interop:xmlns:form:1.0" xmlns:css3t="http://www.w3.org/TR/css3-text/" office:version="1.2" office:mimetype="application/vnd.oasis.opendocument.spreadsheet">
+    <office:body>
+      <office:spreadsheet>
+      <table:calculation-settings table:automatic-find-labels="false" table:use-regular-expressions="false" table:use-wildcards="true"/>
+      <table:table table:name="My Library">
+  `.trim())
+  for (const header of headers) {
+    Zotero.write('<table:table-column/>')
+  }
+
   Zotero.write(make_row(headers))
 
   for (const item of items) {
     Zotero.write(make_row(headers.map(header => item[header])))
   }
+
+  Zotero.write(`
+          </table:table>
+        </office:spreadsheet>
+      </office:body>
+    </office:document>
+  `)
 }
 
 /*
